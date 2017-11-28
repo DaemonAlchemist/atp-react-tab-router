9bd3c5619f7f74021c3e7fd9b234e77cbae43aec
Andrew Wittrock <andy@wittrock.us>
2017-11-28 16:47:57 -0600

    Add module routes

--- src/components/tab-router.jsx
+++ src/components/tab-router.jsx
@@ -3,24 +3,27 @@ import React from "react";
 import {Tabs, Tab} from "react-bootstrap";
 import {Link} from 'react-router-dom';
 
-export default ({tabs, selectedIndex, onSelect, onClose}) =>
-    <Tabs
-        activeKey={selectedIndex}
-        onSelect={() => {}}
-        id="tabPanel"
-    >
-        {tabs.map((tab, index) =>
-            <Tab
-                key={index}
-                eventKey={index}
-                title={
-                    <span>
-                        <Link to={tab.path} onClick={onSelect(index)}>
-                            {tab.label}&nbsp;&nbsp;
-                        </Link>
-                        <i className="fa fa-times" onClick={onClose(index, tab.path)} />
-                    </span>
-                }
-            />
-        )}
-    </Tabs>;
+export default ({tabs, selectedIndex, onSelect, onClose, routes}) =>
+    <div>
+        <Tabs
+            activeKey={selectedIndex}
+            onSelect={() => {}}
+            id="tabPanel"
+        >
+            {tabs.map((tab, index) =>
+                <Tab
+                    key={index}
+                    eventKey={index}
+                    title={
+                        <span>
+                            <Link to={tab.path} onClick={onSelect(index)}>
+                                {tab.label}&nbsp;&nbsp;
+                            </Link>
+                            <i className="fa fa-times" onClick={onClose(index, tab.path)} />
+                        </span>
+                    }
+                />
+            )}
+        </Tabs>
+        {routes}
+    </div>;
